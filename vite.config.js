ca8025ced0d27fc7437b9aa4f24204e83301761a
Luka <xyluxo@gmail.com>
2026-03-30 23:24:09 -0700

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // For GitHub Pages: set to your repo name
+  // e.g. https://lukaluxo.github.io/ebm-explorer/
+  base: '/ebm-explorer/',
   server: {
     port: 3000,
   },
